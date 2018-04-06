c0c529128b450af332e4fc2e501c51fab27d2f26
NERDYLIZARD <bouasavanhhop@gmail.com>
2018-04-06 15:08:36 +0530

    reducer test

---
+++ src/card/cardReducer.spec.js
@@ -0,0 +1,35 @@
+/**
+ * Created on 06-Apr-18.
+ */
+
+import * as actions from './cardActions';
+import reducer from './cardReducer';
+import initialState from '../app/initialState';
+
+describe('Card Reducer', () => {
+
+  it('handle unknown action', () => {
+    const action = {type: 'UNKNOWN'};
+    const expected = initialState.cards;
+    expect(reducer(undefined, action)).toEqual(expected);
+  });
+
+  it('LOAD_CARD_SUCCESS action', () => {
+    const cards = [
+      {
+        wisdom: 'abc',
+        attribute: '123'
+      },
+      {
+        wisdom: 'def',
+        attribute: '456'
+      }
+    ];
+    const action = actions.loadCardsSuccess(cards);
+    const newState = reducer(initialState.cards, action);
+
+    expect(newState).toEqual(cards);
+  });
+
+
+});
